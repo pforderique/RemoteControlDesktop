8320633caadf233157c8b950e4716e7e39a14e23
Piero Orderique <porderiq@mit.edu>
2022-06-11 14:11:38 -0400

    working keyboard toggle

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res, next) {
+  // res.sendFile(path.join(__dirname, 'public', 'index.html'));
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
--- src/components/Home.component.js
+++ src/components/Home.component.js
@@ -1,103 +1,37 @@
 import React, { Component } from "react";
 import KeyboardField from "./KeyboardField.component";
-import { throttle } from "lodash";
-import socket from "../utils/socket";
+import MouseContainer from "./MouseContainer";
 
-const buttons = {
-  0: "left",
-  1: "right",
-  2: "wheel",
-};
+import "./Home.css";
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.lastX = null;
-    this.lastY = null;
-    this.timeOut = null;
-    this.handleTouchMove = throttle(this.NhandleTouchMove, 10);
+    this.state = { showKeyboard: false };
+
+    // This binding is necessary to make `this` work in the callback
+    this.toggleKeyboard = this.toggleKeyboard.bind(this);
   }
 
-  handleMouseEnter = ({ clientX, clientY }) => {
-    this.lastX = clientX;
-    this.lastY = clientY;
-  };
-
-  handleMouseMove = ({ clientX, clientY }) => {
-    this.handleMouse(clientX, clientY);
-  };
-
-  handleMouse = (X, Y, scroll) => {
-    const scrollFactor = 2.2;
-    const x = scrollFactor*(X - this.lastX);
-    const y = scrollFactor*(Y - this.lastY);
-    this.lastX = X;
-    this.lastY = Y;
-    socket.mouseMove({ x, y, scroll });
-  };
-
-  handleTouchStart = (event) => {
-    event.persist();
-    const touchPos = event.touches[0];
-    this.lastX = touchPos.clientX;
-    this.lastY = touchPos.clientY;
-    clearTimeout(this.timeOut);
-    this.timeOut = setTimeout(() => event.preventDefault(), 100);
-  };
-
-  handleTouchEnd = () => {
-    clearTimeout(this.timeOut);
-  };
-
-  NhandleTouchMove = (event) => {
-    event.preventDefault();
-    let touchPos = event.touches[0];
-    const isScroll = event.touches.length > 1;
-    this.handleMouse(touchPos.clientX, touchPos.clientY, isScroll);
-  };
-
-  handleClick = (event) => {
-    socket.mouseClick({ button: buttons[event.button], double: false });
-  };
-
-  handleDoubleClick = (event) => {
-    socket.mouseClick({ button: buttons[event.button], double: true });
-  };
-
-  handleClickBtn = (btn) => {
-    socket.mouseClick({ button: btn, double: true });
-  };
+  toggleKeyboard() {
+    this.setState((prevState) => ({
+      showKeyboard: !prevState.showKeyboard,
+    }));
+  }
 
   render() {
     return (
       <div className="home-container">
-        <KeyboardField></KeyboardField>
-        <div
-          className="touch-container"
-          onMouseEnter={this.handleMouseEnter}
-          onMouseMove={this.handleMouseMove}
-          onTouchStart={this.handleTouchStart}
-          onTouchEnd={this.handleTouchEnd}
-          onTouchMove={this.handleTouchMove}
-          onDoubleClick={this.handleDoubleClick}
-          onClick={this.handleClick}
-          onKeyPressCapture={this.handleLongPress}
-        >
-          {/* <img width="50" height="50" alt="mouse" src="/images/mouse.png"></img> */}
-          {/* make this into a button that pops up the keyboard! */}
-          <img width="100" height="50" alt="keyboard" src="/images/key.png"></img>
-        </div>
-        <div className="buttons">
-          <button className="btn" onClick={() => this.handleClickBtn("left")}>
-            Left
-          </button>
-          <button className="btn" onClick={() => this.handleClickBtn("wheel")}>
-            Wheel
-          </button>
-          <button className="btn" onClick={() => this.handleClickBtn("right")}>
-            Right
-          </button>
-        </div>
+        <button className="keyboard-toggle" onClick={this.toggleKeyboard}>
+          <img
+            width="100"
+            height="50"
+            alt="keyboard"
+            src="/images/key.png"
+          ></img>
+        </button>
+        <MouseContainer retracted={this.state.showKeyboard} />
+        {this.state.showKeyboard && <KeyboardField />}
       </div>
     );
   }
